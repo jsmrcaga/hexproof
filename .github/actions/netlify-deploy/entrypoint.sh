22c8004078b2f4415d7ab6fbbd738799e94aa762
Jo Colina <jo.colina@weezevent.com>
2020-01-26 16:56:37 +0100

    GitHub Actions: docker deploy to netlify

---
+++ .github/actions/netlify-deploy/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm i -g netlify-cli
+
+NETLIFY_AUTH_TOKEN=$1
+BUILD_DIRECTORY=$2
+FUNCTIONS_DIRECTORY=$3
+
+npm run build
+
+# Export token to use with netlify's cli
+export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
+
+# Deploy with netlify
+netlify deploy --dir=$BUILD_DIRECTORY --functions=FUNCTIONS_DIRECTORY
